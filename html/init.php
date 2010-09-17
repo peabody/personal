31cc00c5245987cd73b1d099603f044ee5a5ba00
Sam Peterson <peabodyenator@gmail.com>
2010-09-17 15:42:10 -0700

    Cleaned up unused pages and fixed up some titles and html.

--- html/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_log("site.log");
-error_reporting(E_ALL);
-session_start();
-if (!isset($_SESSION['css'])) {
-    $_SESSION['css'] = '<link rel="stylesheet" type="text/css" href="/site.css">';
-}
-
-?>
