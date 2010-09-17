8b9b5550c05c42a44140a9fc801a55faf0654205
Sam Peterson <peabodyenator@gmail.com>
2010-09-17 14:58:26 -0700

    Added changes to the table of contents Javascript code that now provides a link back to the table of contents when scrolled anywhere else on the page accept the top.

--- html/javascript/table_of_contents.js
+++ html/javascript/table_of_contents.js
@@ -1,7 +1,9 @@
+
+// render table of contents
 $(document).ready(function() {
    var contents = $('<div><ul></ul></div>');
    var tocDiv = $("#toc");
-   tocDiv.append($("<h3>Table of Contents</h3>"));
+   tocDiv.append($("<a name=\"toc\"><h3>Table of Contents</h3></a>"));
    tocDiv.append(contents);
    var list = contents.children("ul");
    $("h2").each(function(i, elm) {
@@ -9,5 +11,18 @@ $(document).ready(function() {
        var anchorText = escape(headerText);
        $(elm).wrap('<a name="' + anchorText + '" />');
        list.append($('<li><a href="#' + anchorText + '">' + headerText + '</a></li>'));
+   });
+});
+
+// provide always on toc link
+$(document).ready(function() {
+   var element = $('<div style="position: fixed; top: 10px; left: 10px; display: none"><a href="#toc">Table of Contents</a></div>');
+   $("body").append(element);
+   $(document).scroll(function() {
+     if($(document).scrollTop() != 0) {
+       element.show();
+     } else {
+       element.hide();
+     }
    });
 });
